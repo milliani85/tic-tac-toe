3235a497300842e7451ff6deb969ad6ac85bd3c7
Robert Miller <robertmiller85@live.co.uk>
2023-03-14 11:30:37 +0000

    Add functions to decide outcome of the game and announce winner

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ const gameboard = (() => {
     const gameboardArray = ['', '', '', '', '', '', '', '', ''];
     const playerOneMark = 'X';
     const playerTwoMark = 'O';
-    let mark = "";
+    let currentMark = "";
 
     // Add choice to gameboard array and board
-    function addChoiceArray(index) {
-        gameboardArray[index] = playerTurn(mark);
+    function addChoiceArray(index, currentMark) {
+        gameboardArray[index] = controlFlow.playerTurn(currentMark);
         grid[index].innerText = gameboardArray[index];
-        console.log(gameboardArray);
     }
           
     // Listen for which cell is clicked
@@ -21,31 +20,111 @@ const gameboard = (() => {
         cell.addEventListener('click', (e) => {
             const index = e.target.id;
             if (e.target.innerText === "") {
-                addChoiceArray(index);
+                addChoiceArray(index, gameboard.currentMark);
             }
+            controlFlow.winner();
+            controlFlow.draw();
         })
     })
 
-    // Controls the player turn.
-    function playerTurn(previousChoice) {
-        if (previousChoice === "") {
-            mark = playerOneMark
-            return mark;
-        } else if (previousChoice === "X") {
-            mark = playerTwoMark
-            return mark;
-        } else if (previousChoice === "O") {
-            mark = playerOneMark
-            return mark;
-        }
-    }
+    return {
+        gameboardArray,
+        currentMark,
+        playerOneMark,
+        playerTwoMark,
+        grid
+    };
+    
 })()
-
-
-
 
 
 // PLAYERS
 
 
 // CONTROL FLOW
+
+const controlFlow = (() => {
+
+    const gameUpdates = document.querySelector('.game-updates');
+
+    // Array of winning combinations.
+    const winVariations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    // Controls the players turn.
+    function playerTurn(previousChoice) {
+        if (previousChoice === "") {
+        gameboard.currentMark = gameboard.playerOneMark;
+        return gameboard.currentMark;
+        } else if (previousChoice === "X") {
+        gameboard.currentMark = gameboard.playerTwoMark;
+        return gameboard.currentMark;
+        } else if (previousChoice === "O") {
+        gameboard.currentMark = gameboard.playerOneMark;
+        return gameboard.currentMark;
+        }
+    }
+
+    // Function to analyze if there has been a winner after each turn.
+    function winner() {
+        for (let i = 0; i < winVariations.length; i++) {
+            const [a, b, c] = winVariations[i];
+            if ((gameboard.gameboardArray[a] === 'X' || gameboard.gameboardArray[a] === 'O')
+                && gameboard.gameboardArray[b] === gameboard.gameboardArray[a]
+                && gameboard.gameboardArray[c] === gameboard.gameboardArray[a]) {
+                announceWinner(gameboard.gameboardArray[a]);
+                highlightWinRow([a, b, c]);
+            } 
+        }
+    }    
+
+    // Function to analyze if the game has finished as a draw.
+    function draw() {
+        let isDraw = true;
+            for (let i = 0; i < gameboard.gameboardArray.length; i++) {
+                if (gameboard.gameboardArray[i] !== 'O' && gameboard.gameboardArray[i] !== 'X') {
+                    isDraw = false;
+                    break;
+                }            
+            }
+            if (isDraw && !gameUpdates.innerText.includes('winner')) {
+                announceDraw();
+    }
+    }
+
+    // Announces winner.
+    function announceWinner(winningMark) {
+        gameUpdates.innerText = `${winningMark} is the winner!`
+    }
+
+    // Announces draw.
+    function announceDraw() {
+        gameUpdates.innerText = 'This game is a draw!'
+    }
+    
+    function highlightWinRow(cellIndexes) {
+        cellIndexes.forEach(index => {
+            gameboard.grid[index].style.color = '#39ff14';
+            gameboard.grid[index].style.fontSize = '5rem'
+        });
+    }
+        
+
+
+    return {
+        playerTurn,
+        winner,
+        draw
+    }
+
+
+})();
+
