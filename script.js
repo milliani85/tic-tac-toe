83eaf4c951ab29a279d30fe62c5ecfd335903bef
Robert Miller <robertmiller85@live.co.uk>
2023-03-15 11:18:58 +0000

    Add function to update score, move to next round and restart game

--- script.js
+++ script.js
@@ -4,27 +4,66 @@
 
 const gameboard = (() => {
     const grid = document.querySelectorAll('.cell');
-    const gameboardArray = ['', '', '', '', '', '', '', '', ''];
+    const restart = document.querySelector('.restart');
+    const nextRoundButton = document.querySelector('.next-round');
+
+    let gameboardArray = ['', '', '', '', '', '', '', '', ''];
     const playerOneMark = 'X';
     const playerTwoMark = 'O';
-    let currentMark = "";
+
+    let currentMark = '';
+
+    enableGrid();
 
     // Add choice to gameboard array and board
     function addChoiceArray(index, currentMark) {
         gameboardArray[index] = controlFlow.playerTurn(currentMark);
         grid[index].innerText = gameboardArray[index];
     }
-          
-    // Listen for which cell is clicked
-    grid.forEach(cell => {
-        cell.addEventListener('click', (e) => {
-            const index = e.target.id;
-            if (e.target.innerText === "") {
-                addChoiceArray(index, gameboard.currentMark);
-            }
-            controlFlow.winner();
-            controlFlow.draw();
+    
+    function enableGrid() {
+        grid.forEach(cell => {
+            cell.addEventListener('click', handleClick)
         })
+    }
+    
+    // Disable cell selection.
+    function disableGrid() {
+        grid.forEach(cell => {
+            cell.removeEventListener('click', handleClick);
+        });
+    }
+
+    // Check if cell hasn't already been played.
+    function handleClick(e) {
+        const index = e.target.id;
+        if (e.target.innerText === '') {
+            addChoiceArray(index, gameboard.currentMark);
+        }
+        controlFlow.winner(gameboardArray);
+        controlFlow.draw();
+    }
+
+    nextRoundButton.addEventListener('click', () => {
+        gameboardArray = ['', '', '', '', '', '', '', '', ''];
+        grid.forEach(cell => {
+            cell.innerText = '';
+            cell.style.color = 'white';
+        })
+        controlFlow.gameUpdates.innerText = '';
+        enableGrid();
+    })
+
+    // Restart current game.
+    restart.addEventListener('click', () => {
+        gameboardArray = ['', '', '', '', '', '', '', '', ''];
+        grid.forEach(cell => {
+            cell.innerText = '';
+            cell.style.color = 'white';
+        })
+        controlFlow.gameUpdates.innerText = '';
+        enableGrid();
+        controlFlow.resetScore();
     })
 
     return {
@@ -32,7 +71,8 @@ const gameboard = (() => {
         currentMark,
         playerOneMark,
         playerTwoMark,
-        grid
+        grid,
+        disableGrid
     };
     
 })()
@@ -46,6 +86,12 @@ const gameboard = (() => {
 const controlFlow = (() => {
 
     const gameUpdates = document.querySelector('.game-updates');
+    let playerOneScoreboard = document.querySelector('.playerone-score');
+    let playerTwoScoreboard = document.querySelector('.playertwo-score');
+
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+
 
     // Array of winning combinations.
     const winVariations = [
@@ -61,32 +107,35 @@ const controlFlow = (() => {
 
     // Controls the players turn.
     function playerTurn(previousChoice) {
-        if (previousChoice === "") {
+        if (previousChoice === '') {
         gameboard.currentMark = gameboard.playerOneMark;
         return gameboard.currentMark;
-        } else if (previousChoice === "X") {
+        } else if (previousChoice === 'X') {
         gameboard.currentMark = gameboard.playerTwoMark;
         return gameboard.currentMark;
-        } else if (previousChoice === "O") {
+        } else if (previousChoice === 'O') {
         gameboard.currentMark = gameboard.playerOneMark;
         return gameboard.currentMark;
         }
     }
 
-    // Function to analyze if there has been a winner after each turn.
-    function winner() {
+    // Analyze if there has been a winner after each turn.
+    function winner(gameboardArray) {
+        console.log(gameboardArray)
         for (let i = 0; i < winVariations.length; i++) {
             const [a, b, c] = winVariations[i];
-            if ((gameboard.gameboardArray[a] === 'X' || gameboard.gameboardArray[a] === 'O')
-                && gameboard.gameboardArray[b] === gameboard.gameboardArray[a]
-                && gameboard.gameboardArray[c] === gameboard.gameboardArray[a]) {
-                announceWinner(gameboard.gameboardArray[a]);
+            if ((gameboardArray[a] === 'X' || gameboardArray[a] === 'O')
+                && gameboardArray[b] === gameboardArray[a]
+                && gameboardArray[c] === gameboardArray[a]) {
+                announceWinner(gameboardArray[a]);
                 highlightWinRow([a, b, c]);
+                stopClick();
+                updateScore();
             } 
         }
     }    
 
-    // Function to analyze if the game has finished as a draw.
+    // Analyze if the game has finished as a draw.
     function draw() {
         let isDraw = true;
             for (let i = 0; i < gameboard.gameboardArray.length; i++) {
@@ -97,7 +146,16 @@ const controlFlow = (() => {
             }
             if (isDraw && !gameUpdates.innerText.includes('winner')) {
                 announceDraw();
+                stopClick();
+                updateScore();
+        }
     }
+
+    // Stop any further selections after game has finished.
+    function stopClick() {  
+        if (gameUpdates.innerText.includes('winner') || gameUpdates.innerText.includes('draw')) {
+            gameboard.disableGrid()
+        }
     }
 
     // Announces winner.
@@ -110,19 +168,38 @@ const controlFlow = (() => {
         gameUpdates.innerText = 'This game is a draw!'
     }
     
+    // Highlights winning row.
     function highlightWinRow(cellIndexes) {
         cellIndexes.forEach(index => {
             gameboard.grid[index].style.color = '#39ff14';
             gameboard.grid[index].style.fontSize = '5rem'
         });
     }
-        
 
+    // Control player scores
+    function updateScore() {
+        if (gameUpdates.innerText.includes('X')) {
+            playerOneScore++;
+            playerOneScoreboard.innerText = playerOneScore;
+        } else if (gameUpdates.innerText.includes('O')) {
+            playerTwoScore++;
+            playerTwoScoreboard.innerText = playerTwoScore;
+        }
+    }
+
+    function resetScore() {
+        playerOneScore = 0;
+        playerTwoScore = 0;
+        playerOneScoreboard.innerText = playerOneScore;
+        playerTwoScoreboard.innerText = playerTwoScore;
+    }
 
     return {
         playerTurn,
         winner,
-        draw
+        draw,
+        resetScore,
+        gameUpdates
     }
 
 
